6ef41203b01b883c98cf6faefb020dbfef28d473
Kanggwanju <xprtktm1@gmail.com>
2025-06-26 10:00:19 +0900

    생성자

---
+++ src/chap1_3/VehicleMain.java
@@ -0,0 +1,11 @@
+package chap1_3;
+
+public class VehicleMain {
+    public static void main(String[] args) {
+        Vehicle grandeur = new Vehicle();
+        Vehicle panamera = new Vehicle();
+
+        System.out.println("grandeur = " + grandeur);
+        System.out.println("panamera = " + panamera);
+    }
+}
