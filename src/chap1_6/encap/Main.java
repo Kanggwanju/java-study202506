dc5f66425f07e3fb133a9f1ecb00b34867c79bcb
Kanggwanju <xprtktm1@gmail.com>
2025-06-30 10:56:31 +0900

    캡슐화 예제 프로그램 구현 및 접근 제어자 이해를 위한 클래스 추가

---
+++ src/chap1_6/encap/Main.java
@@ -0,0 +1,18 @@
+package chap1_6.encap;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        Car myCar = new Car("모닝");
+        // 시동 걸기 위해서 먼저 엔진오일 분사
+        // 시동 버튼 누르고 -> 시동 걸기 -> 실린더 작동
+        myCar.pressButton();
+
+//        myCar.model = "페라리488";
+        // 기어 변속
+        myCar.setMode("N");
+        System.out.println("현재 모드: " + myCar.getMode());
+
+    }
+}
---
+++ src/chap1_6/modi/pac2/C.java
@@ -0,0 +1,22 @@
+package chap1_6.modi.pac2;
+
+import chap1_6.modi.pac1.A;
+
+public class C {
+
+    C() {
+        A a = new A();
+//        new A(200);
+//        new A(6.66);
+
+        a.f1 = 100;
+//        a.f2 = 100;
+//        a.f3 = 100;
+
+        a.m1();
+//        a.m2();
+//        a.m3();
+
+//        new B();
+    }
+}
