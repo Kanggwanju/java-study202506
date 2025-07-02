3664ad6153e665067b70873b6e55963b56ec4915
Kanggwanju <xprtktm1@gmail.com>
2025-07-02 11:05:41 +0900

    다형성 예제 클래스 확장 및 메서드 로직 추가

---
+++ src/chap1_8/poly/casting/emp/Employee.java
@@ -0,0 +1,8 @@
+package chap1_8.poly.casting.emp;
+
+public class Employee {
+
+    public void work() {
+        System.out.println("직원이 출근합니다.");
+    }
+}
---
+++ src/chap1_8/poly/casting/emp/Main.java
@@ -0,0 +1,11 @@
+package chap1_8.poly.casting.emp;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        Owner owner = new Owner();
+        owner.orderWork(new Developer());
+        owner.orderWork(new Designer());
+    }
+}
