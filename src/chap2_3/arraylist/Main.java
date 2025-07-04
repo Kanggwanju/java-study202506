36c6cd96c3712267e8b67bb9d5a9a3466c5ad951
Kanggwanju <xprtktm1@gmail.com>
2025-07-04 12:05:02 +0900

    StringList 클래스 메서드 접근 제어자 수정 및 Main 클래스 추가 및 구현

---
+++ src/chap2_3/arraylist/Main.java
@@ -0,0 +1,14 @@
+package chap2_3.arraylist;
+
+import chap1_3.arraylist.StringList;
+
+public class Main {
+
+    public static void main(String[] args) {
+        StringList foods = new StringList();
+        foods.push("짜장면");
+        foods.push("짬뽕");
+
+        System.out.println(foods);
+    }
+}
